01e5da8320e19c558e8ae7e07d99e328724c6d83
Joel <jw.konitzer@gmail.com>
2023-10-24 08:49:43 -0400

    updated processing

--- generative/process.py
+++ generative/process.py
@@ -12,55 +12,46 @@ config = configparser.ConfigParser()
 config.read('./generative/config.ini')
 params = config["params"]
 
-def main(data):
+def prepare_data(data):
     data["text_clean"] = data["posted_comment"].apply(remove_urls).apply(remove_whitespace).apply(pad_punctuation).astype(str)
-    data = data[data['text_clean'].notnull()]
-    text_data = [text[:int(params['max_len'])] for text in data['text_clean'].tolist()]
+    return data[data['text_clean'].notnull()]
 
-    text_data = [text.lower() for text in text_data]
+def get_datasets(text_data):
     text_data = tf.data.Dataset.from_tensor_slices(text_data)
-
-    # Adapt a temporary TextVectorization layer to your data
-    temp_vectorize_layer = TextVectorization(standardize=None)
-    temp_vectorize_layer.adapt(text_data)
-    dataset_vocab = set(temp_vectorize_layer.get_vocabulary())
-
-    # Remove reserved tokens from dataset vocab
-    dataset_vocab.discard("")
-    dataset_vocab.discard("[UNK]")
-
-    # Combine GloVe and dataset vocabularies (no duplicates)
-    combined_vocab = ["", "[UNK]"] + list(dataset_vocab)[:int(params['vocab_size']) - 2]
-
-    # Create the TextVectorization layer
-    vectorize_layer = TextVectorization(
-        standardize=None,
-        max_tokens=int(params['vocab_size']),
-        output_mode="int",
-        output_sequence_length=int(params['max_len']) + 1,
-    )
-
-    # Set the vocabulary
-    vectorize_layer.set_vocabulary(combined_vocab)
-
-    # Calculate the number of samples
-    n_samples = len(text_data)
-
-    # Calculate the number of samples in each set
-    n_train_samples = int(n_samples * 0.70)  # 70% of data for training
-    n_val_samples = int(n_samples * 0.20)  # 20% of data for validation
-    n_test_samples = n_samples - n_train_samples - n_val_samples  # 10% of data for testing
-
-    # Split the dataset into train, validation, and test sets
+    n_samples = sum(1 for _ in text_data)
+    
+    # Split into training, validation, and test sets
+    n_train_samples = int(n_samples * 0.7)
+    n_val_samples = int(n_samples * 0.2)
+    
     train_text_ds = text_data.take(n_train_samples)
     val_text_ds = text_data.skip(n_train_samples).take(n_val_samples)
     test_text_ds = text_data.skip(n_train_samples + n_val_samples)
 
-    # Shuffle and batch each set individually
-    train_text_ds = train_text_ds.shuffle(buffer_size=n_train_samples).batch(int(params['batch_size']))
-    val_text_ds = val_text_ds.shuffle(buffer_size=n_val_samples).batch(int(params['batch_size']))
-    test_text_ds = test_text_ds.shuffle(buffer_size=n_test_samples).batch(int(params['batch_size']))
+    # Shuffle and batch
+    batch_size = config.getint("params", "batch_size")
+    train_text_ds = train_text_ds.batch(batch_size).shuffle(buffer_size=n_train_samples)
+    val_text_ds = val_text_ds.batch(batch_size).shuffle(buffer_size=n_val_samples)
+    test_text_ds = test_text_ds.batch(batch_size).shuffle(buffer_size=n_samples - n_train_samples - n_val_samples)
 
+    return train_text_ds, val_text_ds, test_text_ds
+    
+def main(data):
+    clean_data = prepare_data(data)
+    text_data = [text[:config.getint("params", "max_len")] for text in clean_data['text_clean'].tolist()]
+    text_data = [text.lower() for text in text_data]
+    
+    # Adapt the TextVectorization layer
+    vectorize_layer = TextVectorization(
+        standardize=None,
+        max_tokens=config.getint("params", "vocab_size"),
+        output_mode="int",
+        output_sequence_length=config.getint("params", "max_len") + 1
+    )
+    
+    train_text_ds, val_text_ds, test_text_ds = get_datasets(text_data)
+    vectorize_layer.adapt(train_text_ds)
+    
     def prepare_lm_inputs_labels(text):
         """
         Shift word sequences by 1 position so that the target for position (i) is
@@ -73,17 +64,17 @@ def main(data):
         y = tokenized_sentences[:, 1:]
         return x, y
 
-    # Prepare the datasets for language model training
     train_ds = train_text_ds.map(prepare_lm_inputs_labels)
     val_ds = val_text_ds.map(prepare_lm_inputs_labels)
     test_ds = test_text_ds.map(prepare_lm_inputs_labels)
 
-    # Prefetch the datasets
     train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
     val_ds = val_ds.prefetch(tf.data.AUTOTUNE)
     test_ds = test_ds.prefetch(tf.data.AUTOTUNE)
-    return train_ds, val_ds, test_ds, combined_vocab
+    
+    # Get the vocabulary
+    vocab = vectorize_layer.get_vocabulary()
+    return train_ds, val_ds, test_ds, vocab
     
 if __name__ == "__main__":
-    data = pd.read_parquet('./data.parquet').sample(5000)  # Replace with different ranges or filtered data - see notebook for more detail
     main(data)
