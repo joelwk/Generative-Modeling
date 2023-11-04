af7bf149108f0b557bed02415a1afb8dd22d4660
Joel <jw.konitzer@gmail.com>
2023-11-04 05:11:49 -0400

    update processing scripts

--- generative_text/general_chat_custom/processing.py
+++ generative_text/general_chat_custom/processing.py
@@ -10,53 +10,47 @@ import configparser
 from generative_text.general_chat_custom.preprocessing import DirectoryManager  
 from generative_text.general_chat_custom.preprocessing import initialize_and_prepare  
 
-def process_and_load_data(replies):
-    # Constants
+
+def process_and_load_data(replies, save_to_disk=True):
+    # Load configuration settings
     config = configparser.ConfigParser()
     config.read('./generative_text/configcustom.ini')
-    config_params = config['params']
-    params = {key: config_params[key] for key in config_params}
-    config_paths = config['paths']
-    paths = {key: config_paths[key] for key in config_paths}
+    params = {key: config['params'][key] for key in config['params']}
+    paths = {key: config['paths'][key] for key in config['paths']}
     base_directory = paths['metadata_path']
     validation_split = float(params['validation_split'])
     max_len = int(params['max_len'])
-    vocab_size = int(params['vocab_size'])
-    embedding_dim = int(params['embedding_dim'])
-    num_heads = int(params['n_heads'])
-    num_layers = int(params['n_layers'])
-    key_dim = int(params['key_dim'])
-    ff_dim = int(params['feed_forward_dim'])
-    dropout_rate = float(params['dropout'])
-    warmup_steps = int(params['warmup_steps'])
-    activation = params['activation']
+    config_vocab_size = int(params['vocab_size']) 
     random_seed = int(params['random_seed'])
-    buffer_size= int(params['buffer_size'])
-    batch_size= int(params['batch_size'])
-    pre_batch_size= int(params['pre_batch_size'])
-    epsilon = 1e-6 
+    buffer_size = int(params['buffer_size'])
+    batch_size = int(params['batch_size'])
+    pre_batch_size = int(params['pre_batch_size'])
     config_directories = DirectoryManager.generate_config(base_directory)
-    DirectoryManager.create_directories(config_directories) 
+    DirectoryManager.create_directories(config_directories)
     meta_data_dir = config_directories['meta_data_dir']
 
+    # Load the data
     text_pairs, voc_comment, voc_response_comment = initialize_and_prepare(base_directory, replies)
 
-    def format_dataset(comment, comment_target, comment_vectorizer, target_comment_vectorizer):
-        comment = comment_vectorizer(comment)
-        comment_target = target_comment_vectorizer(comment_target)
-        # No padding is done here; handle dynamically within the model or during batching
-        encoder_input = comment
-        decoder_input = tf.concat([[0], comment_target[:-1]], axis=0)  # Assuming 0 is the start token
-        target = comment_target
+    def format_dataset(comment_x, response_comment_y, comment_vectorizer, response_comment_vectorizer):
+        encoder_input = comment_vectorizer(comment_x) 
+        response_comment_target = response_comment_vectorizer(response_comment_y)
+        decoder_input = tf.concat([[1], response_comment_target[:-1]], axis=0)
+        target = response_comment_target
+       # tf.print("Encoder input shape:", tf.shape(encoder_input))
+       # tf.print("Decoder input shape:", tf.shape(decoder_input))
+       # tf.print("Target shape:", tf.shape(target))
         return ({"encoder_inputs": encoder_input, "decoder_inputs": decoder_input}, target)
 
-    def make_dataset(pairs, comment_vectorizer, target_comment_vectorizer):
-        thread_texts, comment_texts = zip(*pairs)
-        dataset = tf.data.Dataset.from_tensor_slices((list(thread_texts), list(comment_texts)))
+    def make_dataset(pairs, comment_vectorizer, response_comment_vectorizer):
+        response_comment_texts, comment_texts = zip(*pairs)
+        dataset = tf.data.Dataset.from_tensor_slices((list(comment_texts), list(response_comment_texts)))
         dataset = dataset.shuffle(buffer_size, seed=random_seed) \
-                        .map(lambda x, y: format_dataset(x, y, comment_vectorizer, target_comment_vectorizer)) \
-                        .padded_batch(batch_size, padded_shapes=({'encoder_inputs': [None], 'decoder_inputs': [None]}, [None])) \
-                        .prefetch(pre_batch_size)
+            .map(lambda x, y: format_dataset(x, y, comment_vectorizer, response_comment_vectorizer), 
+                num_parallel_calls=tf.data.AUTOTUNE) \
+                    .padded_batch(batch_size) \
+                        .prefetch(tf.data.AUTOTUNE)
+       # tf.print("Number of batches in the dataset:", tf.data.experimental.cardinality(dataset))
         return dataset
 
     def prepare_data():
@@ -64,7 +58,6 @@ def process_and_load_data(replies):
         tokenizer = tf.keras.preprocessing.text.Tokenizer(filters="")
         tokenizer.fit_on_texts(input_texts + target_texts)
         vocab_size = len(tokenizer.word_index) + 1
-        # Shuffle and split
         random.seed(random_seed)
         random.shuffle(text_pairs)
         total_samples = len(text_pairs)
@@ -73,77 +66,25 @@ def process_and_load_data(replies):
         train_pairs = text_pairs[:n_train]
         val_pairs = text_pairs[n_train:n_train + n_val]
         test_pairs = text_pairs[n_train + n_val:]
-        
-        # Vectorization
         train_comment_texts = [pair[0] for pair in train_pairs]
         train_response_comment_texts = [pair[1] for pair in train_pairs]
-        input_tokens = set(token for text in input_texts for token in text.split())
-        target_tokens = set(token for text in target_texts for token in text.split())
-        comment_tokens, response_comment_tokens = set(), set()
-        comment_maxlen, response_maxlen = 0, 0
-        for comment, response in text_pairs:
-            comment_tok, response_tok = comment.split(), response.split()
-            comment_maxlen = max(comment_maxlen, len(comment_tok))
-            response_maxlen = max(response_maxlen, len(response_tok))
-            comment_tokens.update(comment_tok)
-            response_comment_tokens.update(response_tok)
-            
+        
         comment_vectorizer = TextVectorization(
             standardize=None,
-            max_tokens=vocab_size,
-            output_mode="int",
-            output_sequence_length=max_len
-        )
-        
+            max_tokens=len(list(voc_comment.word2index.keys())),
+            output_mode="int",)
         response_comment_vectorizer = TextVectorization(
             standardize=None,
-            max_tokens=vocab_size,
-            output_mode="int",
-            output_sequence_length=max_len + 1
-        )
+            max_tokens=len(list(voc_response_comment.word2index.keys())),
+            output_mode="int",)
 
         comment_vectorizer.adapt(train_comment_texts)
         response_comment_vectorizer.adapt(train_response_comment_texts)
-
-        # Serialize the model configurations and weights
-        with open(f'{meta_data_dir}/vectorize.pickle', "wb") as fp:
-            data_to_pickle = {
-                "train": train_pairs,
-                "val": val_pairs,
-                "test": test_pairs,
-                "commentvec_config": comment_vectorizer.get_config(),
-                "commentvec_weights": comment_vectorizer.get_weights(),
-                "commentresponsevec_config": response_comment_vectorizer.get_config(),
-                "commentresponsevec_weights": response_comment_vectorizer.get_weights(),
-            }
-            pickle.dump(data_to_pickle, fp)
-
-        # Save the vocabulary
-        comment_vocab = comment_vectorizer.get_vocabulary()
-        response_comment_vocab = response_comment_vectorizer.get_vocabulary()
-        
-        # Serialize vocabulary
-        with open(f'{meta_data_dir}/comment_vocab.pickle', 'wb') as f:
-            pickle.dump(comment_vocab, f)
-        with open(f'{meta_data_dir}/response_comment_vocab.pickle', 'wb') as f:
-            pickle.dump(response_comment_vocab, f)
         return comment_vectorizer, response_comment_vectorizer, train_pairs, val_pairs, test_pairs
-        
-    if not os.path.exists(os.path.join(meta_data_dir, f"vectorize.pickle")):
+    if not os.path.exists(os.path.join(meta_data_dir, "vectorize.pickle")):
         comment_vectorizer, response_comment_vectorizer, train_pairs, val_pairs, test_pairs = prepare_data()
-        # Serialize the model configurations and weights
-        with open(f'{meta_data_dir}/vectorize.pickle', "wb") as fp:
-            data_to_pickle = {
-                "train": train_pairs,
-                "val": val_pairs,
-                "test": test_pairs,
-                "commentvec_config": comment_vectorizer.get_config(),
-                "commentvec_weights": comment_vectorizer.get_weights(),
-                "commentresponsevec_config": response_comment_vectorizer.get_config(),
-                "commentresponsevec_weights": response_comment_vectorizer.get_weights(),}
-            pickle.dump(data_to_pickle, fp)
     else:
-        with open(os.path.join(meta_data_dir, f"vectorize.pickle"), "rb") as fp:
+        with open(os.path.join(meta_data_dir, "vectorize.pickle"), "rb") as fp:
             data = pickle.load(fp)
         comment_vectorizer = TextVectorization.from_config(data["commentvec_config"])
         comment_vectorizer.set_weights(data["commentvec_weights"])
@@ -152,6 +93,7 @@ def process_and_load_data(replies):
         train_pairs = data["train"]
         val_pairs = data["val"]
         test_pairs = data["test"]
+
     train_ds = make_dataset(train_pairs, comment_vectorizer, response_comment_vectorizer)
     val_ds = make_dataset(val_pairs, comment_vectorizer, response_comment_vectorizer)
     test_ds = make_dataset(test_pairs, comment_vectorizer, response_comment_vectorizer)
